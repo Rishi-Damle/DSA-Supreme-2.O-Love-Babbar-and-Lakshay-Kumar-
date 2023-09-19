d9af6bff3352678d3f596bc945e37946ac62c27e
Rishi Damle <108988060+Rishi-Damle@users.noreply.github.com>
2023-09-18 23:51:46 -0700

    Hey Folks here are the LEVEL-1 Assignment Solution of DSA Supreme Batch 2.O !

---
+++ ClassLoop3.cpp
@@ -0,0 +1,11 @@
+// Print 1 to 100 even numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 2; i < 101; i = i + 2)
+    {
+        cout << i << endl;
+    }
+    return 0;
+}
---
+++ FirstProgram.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello Supreme 2.O";
+    return 0;
+}
